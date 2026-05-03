1f199497abc4575ca0522615a1f9e3f03f65e062
sahilattar8786 <sahilattar87860@gmail.com>
2026-05-03 22:21:28 +0530

    clearK Implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
-import { Stack } from "expo-router";
 import "../global.css";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+import { Stack } from "expo-router";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not set");
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ClerkProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import { useAuth } from "@clerk/expo";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const {isLoaded, isSignedIn} = useAuth()
+  if (!isLoaded)  return null ;
+  if (isSignedIn) return <Redirect href={"/(root)/(tabs)"} />
+
+
+  return <Redirect href={"/sign-in"} />
 }
