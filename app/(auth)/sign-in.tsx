9072ca057d6d4ff5e9eb7f574584cfd71a9fb5a7
sahilattar8786 <sahilattar87860@gmail.com>
2026-05-03 22:40:22 +0530

    initaal setup

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-
+import { Image, ScrollView, Text, View } from 'react-native'
 export default function SignIn() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-2xl font-bold'>SignIn</Text>
-    </View>
+    <ScrollView
+      className='bg-white'
+      contentContainerStyle={{ flexGrow: 1 }}
+      keyboardShouldPersistTaps='handled'
+    >
+      <View className="flex-1 justify-center px-6 py-12">
+        <Image 
+         source={require('../../assets/images/kribb.png')} 
+         className='w-32 h-16 mb-8'
+         resizeMode='contain'
+         />
+         <Text className="text-3xl font-bold text-grey-800 mb-2">Create Account</Text>
+         <Text className="text-grey-500 mb-8">Find Your Dream Home Today</Text>
+      </View>
+    </ScrollView>
   )
 }
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+export default function SignUp() {
+  return (
+    <>
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-2xl font-bold'>SignUp</Text>
+    </View>
+    </>
+  )
+}
